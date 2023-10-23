253a6ce49b107090bcfc86042ee94002eae3eaab
iExploze <54289660+iExploze@users.noreply.github.com>
2023-10-23 16:51:21 -0400

    updated quiz and playlist

--- src/entities/Quiz.java
+++ src/entities/Quiz.java
@@ -1,4 +1,19 @@
 package entities;
 
+import java.util.ArrayList;
+
 public interface Quiz {
+    ArrayList<Player> players();
+
+    Song currentPlaying();
+
+    Song goNext();
+
+    double getTimeLeft();
+
+    int getRemaining();
+
+    boolean isSong(String name);
+
+    int getPoints();
 }
