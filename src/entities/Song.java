55990d1a5ca34f346215b6991f44aa30fcfc4705
Anson Lau <ansonlau57@gmail.com>
2023-10-23 16:41:16 -0400

    Updated Song interface

--- src/entities/Song.java
+++ src/entities/Song.java
@@ -1,4 +1,14 @@
 package entities;
 
 public interface Song {
+    String getArtistName();
+
+    String getSongName();
+
+    String getSongGenre();
+
+    String getNumStreams();
+
+    void playSong();
+    // gets the mp3 file and plays the song
 }
