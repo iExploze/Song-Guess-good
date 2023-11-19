fcd54ff961bd1e7c0d777961f79128b47a78d335
floraguo <flora.guo@mail.utoronto.ca>
2023-11-19 14:59:32 -0500

    Moved on to PlayInteractor and figuring out play function

--- src/main/java/usecase/play_song/PlayInteractor.java
+++ src/main/java/usecase/play_song/PlayInteractor.java
@@ -1,4 +1,21 @@
 package usecase.play_song;
 
-public class PlayInteractor {
+import entities.Player;
+import entities.PlayerFactory;
+public class PlayInteractor implements PlayInputBoundary{
+    final PlayUserDataAccessInterface userDataAccessObject;
+    final PlayOutputBoundary playPresenter;
+    final PlayerFactory playerFactory;
+
+    public PlayInteractor(PlayUserDataAccessInterface userDataAccessInterface,
+                          PlayOutputBoundary playOutputBoundary,
+                          PlayerFactory playerFactory) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.playPresenter = playOutputBoundary;
+        this.playerFactory = playerFactory;
+    }
+
+    public void execute() {
+
+    }
 }
