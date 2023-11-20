e53abffa7bc3cf464785ad3270f2b8104fa2d54e
Anson Lau <ansonlau57@gmail.com>
2023-11-20 13:59:42 -0500

    Modified Main.java to include code for the PlayViewModel. Commented out for now to avoid errors while the PlayUseCaseFactory is incomplete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 //import interface_adapter.play_song.PlayViewModel;
+//import app.PlayUseCaseFactory;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.play_song.PlayViewModel;
 import view.PlayView;
+import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,7 +14,7 @@ public class Main {
         // various cards, and the layout, and stitch them together.
 
         // The main application window.
-        JFrame application = new JFrame("Login Example");
+        JFrame application = new JFrame("Play Example");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         CardLayout cardLayout = new CardLayout();
@@ -20,8 +23,21 @@ public class Main {
         JPanel views = new JPanel(cardLayout);
         application.add(views);
 
-        ViewManagerModel viewManagerModel = new ViewManagerModel();
-
+//        // This keeps track of and manages which view is currently showing.
+//        ViewManagerModel viewManagerModel = new ViewManagerModel();
+//        new ViewManager(views, cardLayout, viewManagerModel);
+//
+//        // The data for the views, such as username and password, are in the ViewModels.
+//        // This information will be changed by a presenter object that is reporting the
+//        // results from the use case. The ViewModels are observable, and will
+//        // be observed by the Views.
+//        PlayViewModel playViewModel = new PlayViewModel();
+//
+//        PlayView playView = PlayUseCaseFactory.create(viewManagerModel, playViewModel, );
+//        views.add(playView, playView.viewName);
+//
+//        viewManagerModel.setActiveView(playView.viewName);
+//        viewManagerModel.firePropertyChanged();
 
         application.pack();
         application.setVisible(true);
