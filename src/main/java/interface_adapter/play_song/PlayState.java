e55e7316db17143cd1fd807b5d915d98ac09c081
Anson Lau <ansonlau57@gmail.com>
2023-11-20 13:13:11 -0500

    Updated the PlayState

--- src/main/java/interface_adapter/play_song/PlayState.java
+++ src/main/java/interface_adapter/play_song/PlayState.java
@@ -1,4 +1,19 @@
 package interface_adapter.play_song;
 
 public class PlayState {
+    private String guess = "";
+
+    public  PlayState(PlayState copy){
+        guess = copy.guess;
+    }
+
+    public PlayState(){}
+
+    public String getGuess(){
+        return guess;
+    }
+
+    public void setGuess(String guess){
+        this.guess = guess;
+    }
 }
