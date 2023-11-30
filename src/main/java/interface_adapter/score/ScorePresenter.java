b87721ed6dfab7c76e297c2728d097b41e2f87b6
iExploze <54289660+iExploze@users.noreply.github.com>
2023-11-29 21:22:49 -0500

    Added interfaces adapters for score

---
+++ src/main/java/interface_adapter/score/ScorePresenter.java
@@ -0,0 +1,10 @@
+package interface_adapter.score;
+
+import usecase.score.ScoreOutputBoundary;
+
+public class ScorePresenter implements ScoreOutputBoundary {
+    @Override
+    public void displayScore() {
+
+    }
+}
